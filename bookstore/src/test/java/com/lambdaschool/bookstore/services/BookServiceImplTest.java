d1d85189706d3f5534cb05cfe0d630c5df580c7e
brandononeal <baoneal95@gmail.com>
2020-11-13 12:11:26 -0800

    testing complete

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,19 +2,27 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
+import com.lambdaschool.bookstore.models.Author;
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
+import com.lambdaschool.bookstore.models.Wrote;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 //**********
 // Note security is handled at the controller, hence we do not need to worry about security here!
 //**********
@@ -23,6 +31,12 @@ public class BookServiceImplTest
 
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private SectionService sectionService;
+
+    @Autowired
+    private AuthorService authorService;
 
     @Before
     public void setUp() throws
@@ -38,33 +52,47 @@ public class BookServiceImplTest
     }
 
     @Test
-    public void findAll() throws Exception
+    public void findAll()
     {
-        assertEquals("Flatterland", bookService.findBookById(26).getTitle());
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
-        //
+        assertEquals("Flatterland", bookService.findBookById(26).getTitle());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void notFindBookById()
     {
-        //
+        bookService.findBookById(23);
     }
 
     @Test
-    public void delete()
+    public void z_delete()
     {
-        //
+        bookService.delete(26);
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void save()
     {
-        //
+        String b6Title = "The Teachings of Don Juan";
+
+        Author a7 = new Author("Carlos", "Castaneda");
+        a7 = authorService.save(a7);
+
+        Section s6 = new Section("Memoir");
+        s6 = sectionService.save(s6);
+
+        Book b6 = new Book ("The Teachings of Don Juan", "9780520002173", 1968, s6);
+        b6.getWrotes().add(new Wrote(a7, new Book()));
+        Book newBook = bookService.save(b6);
+
+        assertNotNull(newBook);
+        assertEquals(b6Title, newBook.getTitle());
     }
 
     @Test
